7136694caf2d8eb130cec7b30c0ebcb029796a7b
Chris Henry <cdhenry@gmail.com>
2018-03-31 14:26:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,12 @@
 class SongsController < ApplicationController
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+     self.artist.name
+  end
+  
   def index
     @songs = Song.all
   end
